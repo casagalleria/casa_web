d5df0be42aefbf96a6d3bb1f85a1babf66b4491b
yeri <parkya200@naver.com>
2016-04-13 17:09:30 +0900

    ,,kk,

---
+++ casa_web/src/main/webapp/js/index.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+	    $(".about").hover(function(){
+			$(".about").text("까사 갤러리아란");
+	    },
+	    function(){
+			$(".about").text("ABOUT US");
+	    });
+	});
+	$(document).ready(function(){
+	    $(".artists").hover(function(){
+			$(".artists").text("작가 보기");
+	    },
+	    function(){
+			$(".artists").text("ARTISTS");
+	    });
+	});
+	$(document).ready(function(){
+	    $(".artworks").hover(function(){
+			$(".artworks").text("작품 보기");
+	    },
+	    function(){
+			$(".artworks").text("ARTWORKS");
+	    });
+	});
+	$(document).ready(function(){
+	    $(".mygalleria").hover(function(){
+			$(".mygalleria").text("마이 갤러리아");
+	    },
+	    function(){
+			$(".mygalleria").text("MY GALLERIA");
+	    });
+	});
+	$(document).ready(function(){
+	    $(".withus").hover(function(){
+			$(".withus").text("함께 하는 분들");
+	    },
+	    function(){
+			$(".withus").text("WITH US");
+	    });
+	});
+	$(document).ready(function(){
+	    $(".rules").hover(function(){
+			$(".rules").text("규정");
+	    },
+	    function(){
+			$(".rules").text("RULES");
+	    });
+	});
